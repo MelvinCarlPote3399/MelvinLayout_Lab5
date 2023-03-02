21214db9f7ea5060ec441f293c533c74670927e2
MelvinCarlPote3399 <melvincarl2000@gmail.com>
2023-03-02 00:03:16 -0500

    Managed to implement switching between fragments, with the help of link of the article that was provided.

--- MelvinModule/src/main/java/melvincarl/pote/n01483399/lab5/PoteActivity.java
+++ MelvinModule/src/main/java/melvincarl/pote/n01483399/lab5/PoteActivity.java
@@ -1,14 +1,34 @@
 package melvincarl.pote.n01483399.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class PoteActivity extends AppCompatActivity {
+
+    ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        viewPager2 = findViewById(R.id.viewPager2);
+
+    }
+    public void setViewPagerAdapter() {
+        ViewPager2Adapter viewPager2Adapter = new ViewPager2Adapter(this);
+        ArrayList<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new HomeFragment());
+        fragmentList.add(new SettingsFragment());
+        fragmentList.add(new FavouritesFragment());
+
+        viewPager2.setAdapter(viewPager2Adapter);
     }
 }
