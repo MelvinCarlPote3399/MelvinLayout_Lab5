2105e3c577449258e061f5f740b8d72a1edcdb86
MelvinCarlPote3399 <melvincarl2000@gmail.com>
2023-03-01 23:51:03 -0500

    Added our adapter (according to the instructions that were stated in the link that was provided to use in the lab document)(correction)

--- MelvinModule/src/main/java/melvincarl/pote/n01483399/lab5/ViewPager2Adapter.java
+++ MelvinModule/src/main/java/melvincarl/pote/n01483399/lab5/ViewPager2Adapter.java
@@ -18,7 +18,7 @@ public class ViewPager2Adapter extends FragmentStateAdapter {
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        return null;
+        return fragments.get(position);
     }
 
     @Override
