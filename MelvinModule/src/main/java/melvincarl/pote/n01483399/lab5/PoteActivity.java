3855e95afef3cd60a9357bb1c529d79929962073
MelvinCarlPote3399 <melvincarl2000@gmail.com>
2023-03-02 00:30:19 -0500

    Managed to create tab labels

--- MelvinModule/src/main/java/melvincarl/pote/n01483399/lab5/PoteActivity.java
+++ MelvinModule/src/main/java/melvincarl/pote/n01483399/lab5/PoteActivity.java
@@ -1,25 +1,37 @@
 package melvincarl.pote.n01483399.lab5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.util.ArrayList;
 
-public class PoteActivity extends AppCompatActivity {
+public class PoteActivity extends AppCompatActivity implements TabLayoutMediator.TabConfigurationStrategy {
 
     ViewPager2 viewPager2;
+    TabLayout tabLayout;
+    ArrayList<String> titles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         viewPager2 = findViewById(R.id.viewPager2);
-
+        tabLayout = findViewById(R.id.tabLayout);
+        titles = new ArrayList<String>();
+        titles.add("Home");
+        titles.add("Settings");
+        titles.add("Favorites");
+        setViewPagerAdapter();
+        new TabLayoutMediator(tabLayout,viewPager2,this).attach();
     }
     public void setViewPagerAdapter () {
         ViewPager2Adapter viewPager2Adapter = new ViewPager2Adapter(this);
@@ -31,4 +43,8 @@ public class PoteActivity extends AppCompatActivity {
         viewPager2.setAdapter(viewPager2Adapter);
     }
 
+    @Override
+    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+        tab.setText(titles.get(position));
+    }
 }
